9d5468001ea6d7220f7e6934ccc21aee6bec4728
Chris Atkinson <46910324+chrisatk@users.noreply.github.com>
2024-02-29 11:06:14 -0500

    Create handler.php

---
+++ handler.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <title>Welcome Page</title>
+  </head>
+
+  <body>
+    
+    <?php
+    $userName = "";
+    if ( isset( $_POST['userName'] ) ){
+      $userName = $_POST['userName'];
+    }
+    echo "<h1>Welcome ".$userName."!</h1>\n";
+    ?>
+    
+  </body>
+  
+</html>
